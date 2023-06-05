9461e4e2c91f4bfce32e5d95df3107621aa38031
Wesley <wesley.leoncio.izi@gmail.com>
2023-06-05 20:39:13 -0300

    criando projeto de teste

---
+++ pokemon/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Banner from './components/banner'
+import CampoTexto from './components/campo-texto'
+
+function App() {
+
+  return (
+    <>
+      <Banner/>
+      <CampoTexto/>
+    </>
+  )
+}
+
+export default App
