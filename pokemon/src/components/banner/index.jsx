af21b6cd6c20dfff90482b109f8068a8d56a61b0
Wesley <wesley.leoncio.izi@gmail.com>
2023-06-06 23:37:25 -0300

    teste 3

--- pokemon/src/components/banner/index.jsx
+++ pokemon/src/components/banner/index.jsx
@@ -2,7 +2,7 @@ import  './banner.css'
 const Banner = () => {
     return (
         <header className="banner">
-            <img src="src/assets/image/banner1.png" alt="O banner principal da página do pokemon" />
+            <img src="/src/assets/image/banner1.png" alt="O banner principal da página do pokemon1" />
         </header>
 
     )
